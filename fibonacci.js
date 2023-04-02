9953b7d7da139d29f6ae6ee283eb4fa8ea1925c2
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-04-02 10:19:44 +0530

    add recursive function

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,20 @@ const fibs = (n) => {
     }
     return resultArray;
 }
+
+const fibs = (n) => {
+    const resultArray = [];
+    for(let i = 0; i <= n; i++){
+        if (i == 0){
+            resultArray.push(0)
+        }
+        else if(i == 1){
+            resultArray.push(1);
+        }
+
+        else{
+            resultArray.push(resultArray[i-1] + resultArray[i-2]);
+        }
+    }
+    return resultArray;
+}
